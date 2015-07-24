7e7b496d0d2e53db6421fd0671566c2ca6ec3c73
Francisco ME <if.francisco.me@gmail.com>
2015-07-24 16:28:46 -0500

    Create SHA256inR.r

---
+++ SHA256inR.r
@@ -0,0 +1,29 @@
+
+# -- -------------------------------------------------------------------------------------------- #
+# -- Initial Developer: IF.Francisco.ME --------------------------------------------------------- #
+# -- sha256 Message content POST ---------------------------------------------------------------- #
+# -- License: GNU License V3 -------------------------------------------------------------------- #
+# -- -------------------------------------------------------------------------------------------- #
+
+if (!require(digest)) install.packages('digest', quiet = TRUE)
+suppressMessages(library (digest))     # SHA-256 Encoding for POST requests
+
+if (!require(httr)) install.packages('httr', quiet = TRUE)
+suppressMessages(library (httr))       # meXBTMktsleer url's
+
+if (!require(jsonlite)) install.packages('jsonlite', quiet = TRUE)
+suppressMessages(library (jsonlite))   # JSON parser
+
+if (!require(RCurl)) install.packages('RCurl', quiet = TRUE)
+suppressMessages(library (RCurl))      # leer url's
+
+UserID <- "franciscome@mexbt.com"
+APIKey <- "123456789abcdefg789hijklmnopqrst" # Both are examples
+PvtKey <- "tsrqponmlkjih987gfedcba987654321" # Place yours here
+
+APInonce   <- trunc(as.numeric(Sys.time()), "miliseconds")
+PreMessage <- paste(get("APInonce"),UserID,sep="")
+Message    <- paste(PreMessage,APIKey,sep="")
+APIMessage <- paste(paste(get("APInonce"),UserID,sep=""),APIKey,sep="")
+
+APISig <- toupper(hmac(object = APIMessage, key=PvtKey,algo="sha256",serialize=FALSE))
